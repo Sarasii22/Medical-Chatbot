40f921c7fc8149b4775eb7dd1a9c90cf27cbdb4d
Sarasi <eg245387@engug.ruh.ac.lk>
2026-04-15 23:28:50 +0530

    do the pipeline

---
+++ src/helper.py
@@ -0,0 +1,43 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from typing import List
+from langchain_core.documents import Document
+
+#Extract text from PDF files
+def load_pdf_file(data):
+    loader = DirectoryLoader(data,
+                             glob="*.pdf",
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+
+#filter to minimal
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata=doc.metadata
+            )
+        )
+        
+    return minimal_docs
+
+#split data into test chunks
+def text_split(extracted_docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(extracted_docs)
+    return text_chunks
+
+
+#download the HuggingFace embedding model
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    return embeddings
---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+system_prompt = (
+    "You are a Medical assistant for question-answering tasks"
+    "Use the following pieces of retrieved documents to answer the question. If you don't know the answer, say you don't know." \
+    "the question, if you don't know the answer, say you don't know. Use three sentences at most to answer the question." \
+    "answer concisely and accurately. "
+    "\n\n"
+    "{context}" 
+)
+
+
